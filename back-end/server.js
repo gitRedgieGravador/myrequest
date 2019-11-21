eda4c7390bd29639a64d8239b4b847346d0124c9
cjcabungcagcls <2ndyrGroupC@pn.local>
2019-11-21 12:14:42 +0800

    sendmail

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const userRoute = require('./routes/user')
-    //var connect = require('./setup')
+var mandrill = require('node-mandrill')('c3d911e4-578e-4933-ab89-97b6de339be9');
+//var connect = require('./setup')
 var cors = require('cors')
 
 app.use(cors())
@@ -19,3 +20,35 @@ io.on("connection", socket => {
         socket.broadcast.emit('sample', data)
     })
 });
+
+// var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.elasticemail.com',
+//     port: 2525,
+//     secure: false,
+//     auth: {
+//         user: 'iderge32@gmail.com',
+//         pass: '12da171b-6a8a-4e74-b66e-b97ac72ccbf2'
+//     },
+//     tls: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+// var mailOptions = {
+//     from: 'iderge32@gmail.com',
+//     to: 'redgie.gravador@student.passerellesnumeriques.org',
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info) {
+//     console.log("sending...")
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
--- front-end/src/modules/sample.vue
+++ front-end/src/modules/sample.vue
@@ -25,7 +25,6 @@ export default {
       this.sample = data;
     },
     onSample() {
-      var event;
       socket.on("sample", data => {
         this.passdata(data);
       });
