f0ea9d4bbe09bf7e62bdc72a78e148718c0e2840
rivasge_sd2023@CEACCS.NET <rivasge_sd2023@CEACCS.NET>
2019-11-23 09:09:37 +0800

    Updated the Form Geneva 11/23/19

--- front-end/src/modules/Form.vue
+++ front-end/src/modules/Form.vue
@@ -7,10 +7,10 @@
             <v-col cols="12" sm="8" md="4">
               <v-card class="elevation-12">
                 <v-toolbar color="primary" dark flat>
-                  <v-toolbar-title>Fill in the following information:</v-toolbar-title>
-                  <v-spacer/>
+                  <v-toolbar-title>Fill in the following information</v-toolbar-title>
+                  <v-spacer />
                 </v-toolbar>
-                <v-card-text>
+                <v-card-text >
                   <v-form>
                     <!-- <v-text-field label="Login" name="login" prepend-icon="mdi-person" type="text"/>
 
@@ -22,18 +22,18 @@
                       type="password"
                     />-->
                     <v-select
-                      v-model="select"
+                      v-model="selectBatch"
                       :items="batch"
-                      :rules="[v => !!v || 'Item is required']"
+                      :rules="[v => !!v || 'Batch is required']"
                       label="Batch"
                       required
                     ></v-select>
                     <v-text-field
                       v-model="name"
                       prepend-icon="mdi-account"
-                      :counter="10"
+                      :counter="25"
                       :rules="nameRules"
-                      label="Name"
+                      label="Full Name"
                       required
                     ></v-text-field>
 
@@ -44,11 +44,36 @@
                       label="E-mail"
                       required
                     ></v-text-field>
+                    <br />
+                    <br />
+                    <v-select
+                      v-model="selectCategory"
+                      :items="category"
+                      :rules="[v => !!v || 'Category is required']"
+                      label="Request Category"
+                      required
+                    ></v-select>
+                    <br />
+                    <v-text-field
+                      v-model="title"
+                      :rules="[v => !!v || 'Title is required']"
+                      prepend-icon="mdi-tag"
+                      label="Request Title"
+                      required
+                    ></v-text-field>
+                    <br />
+                    <v-textarea
+                      outlined
+                      name="input-7-4"
+                      label="Request Description"
+                      value
+                      :rules="[v => !!v || 'Description is required']"
+                    ></v-textarea>
                   </v-form>
                 </v-card-text>
                 <v-card-actions>
-                  <v-spacer/>
-                  <v-btn color="primary">Submit</v-btn>
+                  <v-spacer />
+                  <v-btn color="orange">Submit</v-btn>
                 </v-card-actions>
               </v-card>
             </v-col>
@@ -67,15 +92,23 @@ export default {
       name: "",
       nameRules: [
         v => !!v || "Name is required",
-        v => (v && v.length <= 10) || "Name must be less than 10 characters"
+        v => (v && v.length <= 25) || "Name must be less than 25 characters"
       ],
       email: "",
       emailRules: [
         v => !!v || "E-mail is required",
         v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ],
-      select: null,
-      batch: ["2020", "2021", "2022"]
+      selectBatch: null,
+      batch: ["2020", "2021", "2022"],
+      selectCategory: null,
+      category: [
+        "Personal",
+        "Whole Batch",
+        "Center Supplies",
+        "School Supplies"
+      ],
+      title: ""
     };
   }
 };
